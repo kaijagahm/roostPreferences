82eb578cf6edca3ab86f16c9f8fabf594f30e19b
Kaija Gahm <kgahm@ucla.edu>
2023-04-26 17:19:58 -0700

    plot edits

--- 01_roostPreferences.R
+++ 01_roostPreferences.R
@@ -127,51 +127,56 @@ mns <- randomized %>%
 all <- mns %>%
   left_join(pvals, by = c("ID1", "ID2", "season", "actual")) %>%
   mutate(dyad = paste(ID1, ID2, sep = "_"))
-  
+
 # Just look at one individual, abel, and all his associations
 abel <- randomized %>%
   filter(ID1 == "abel", season == "2022_b")
 
 (abelPrefs <- all %>%
-  filter(ID1 == "abel", season == "2022_b") %>%
-  ggplot()+
-  geom_segment(aes(x = fct_reorder(ID2, desc(diffMnActual)), xend = fct_reorder(ID2, desc(diffMnActual)), y = mn, yend = actual))+
-  geom_point(aes(x = fct_reorder(ID2, desc(diffMnActual)), y = mn), col = "skyblue")+
-  geom_point(aes(x = fct_reorder(ID2, desc(diffMnActual)), y = actual), col = "red")+
-  theme_classic())
-ggsave(abelPrefs, file = "fig/abelPrefs.png")
-
-Jill <- randomized %>%
-  filter(ID1 == "Jill", season == "2022_b")
-
-(JillPrefs <- all %>%
-    filter(ID1 == "Jill", season == "2022_b") %>%
+    filter(ID1 == "abel", season == "2022_b") %>%
     ggplot()+
     geom_segment(aes(x = fct_reorder(ID2, desc(diffMnActual)), xend = fct_reorder(ID2, desc(diffMnActual)), y = mn, yend = actual))+
     geom_point(aes(x = fct_reorder(ID2, desc(diffMnActual)), y = mn), col = "skyblue")+
     geom_point(aes(x = fct_reorder(ID2, desc(diffMnActual)), y = actual), col = "red")+
-    theme_classic())
-ggsave(JillPrefs, file = "fig/JillPrefs.png")
+    theme_classic()+
+    theme(axis.text.x = element_blank())+
+    ylab("Prop. nights together")+
+    xlab("Other individual")+
+    ggtitle("Abel"))
+ggsave(abelPrefs, file = "fig/abelPrefs.png")
 
 pamela <- randomized %>%
   filter(ID1 == "pamela", season == "2022_b")
 
 (pamelaPrefs <- all %>%
-    filter(ID1 == "pamela", season == "2022_b") %>%
+    filter(ID1 == "pamela", season == "2022_b", !is.na(actual)) %>%
     ggplot()+
     geom_segment(aes(x = fct_reorder(ID2, desc(diffMnActual)), xend = fct_reorder(ID2, desc(diffMnActual)), y = mn, yend = actual))+
     geom_point(aes(x = fct_reorder(ID2, desc(diffMnActual)), y = mn), col = "skyblue")+
     geom_point(aes(x = fct_reorder(ID2, desc(diffMnActual)), y = actual), col = "red")+
-    theme_classic())
+    theme_classic()+
+    theme(axis.text.x = element_blank())+
+    ylab("Prop. nights together")+
+    xlab("Other individual")+
+    ggtitle("Pamela"))
 ggsave(pamelaPrefs, file = "fig/pamelaPrefs.png")
 
 # Ran through this with Sean. Questions:
 # 1. Do individuals differ in the overall size of their preferences?
-all %>%
-  ggplot(aes(x = ID1, y = diffMnActual))+
-  geom_boxplot()+
-  theme_classic()+
-  facet_wrap(~season) # ... kinda? but there's just a lot of variability generally.
+(indivPrefs <- all %>%
+    group_by(ID1, season) %>%
+    filter(n() > 5) %>% # only individuals that interacted with at least 5 others
+    mutate(mnPref = mean(diffMnActual, na.rm = T)) %>%
+    ungroup() %>%
+    ggplot(aes(x = fct_reorder(ID1, desc(mnPref)), y = diffMnActual))+
+    geom_boxplot(col = "gray30", fill = "gray30", outlier.size = 0.5, size = 0.5)+
+    theme_classic()+
+    geom_hline(yintercept = 0, col = "red", lty = 2)+
+    facet_wrap(~season, nrow = 3)+ # ... kinda? but there's just a lot of variability generally.
+    theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())+
+    ylab("Proportion differential (actual vs. mean random)")+
+    xlab("Individual"))
+ggsave(indivPrefs, file = "fig/indivPrefs.png", width = 9, height = 5, dpi = 800)
 
 # 2. Do roost locations vary in busyness night to night? E.g. do all individuals prefer the same site on *certain nights* all at the same time?
 rss <- map2(rs, s, ~.x %>% mutate(season = .y)) %>%
@@ -187,25 +192,25 @@ busy <- rss %>%
 
 # proportion of individuals at the site tonight
 (propIndsHereTonight <- busy %>%
-  group_by(roostID) %>%
-  filter(length(unique(roost_date)) > 3) %>% # only include roosts used by someone on at least 3 nights
-  ggplot(aes(x = roost_date, y = propIndsHereTonight, col = roostID))+
-  geom_line()+
-  theme_minimal()+
-  theme(legend.position = "none") +
-  facet_wrap(~season, nrow = 3, scales = "free_x"))
-ggsave(propIndsHereTonight, file = "fig/propIndsHereTonight.png")
+    group_by(roostID) %>%
+    filter(length(unique(roost_date)) > 3) %>% # only include roosts used by someone on at least 3 nights
+    ggplot(aes(x = roost_date, y = propIndsHereTonight, col = roostID))+
+    geom_line()+
+    theme_minimal()+
+    theme(legend.position = "none") +
+    facet_wrap(~season, nrow = 3, scales = "free_x"))
+ggsave(propIndsHereTonight, file = "fig/propIndsHereTonight.png", width = 9, height = 5, dpi = 800)
 
 # number of individuals at the site tonight
 (indsHereTonight <- busy %>%
-  group_by(roostID) %>%
-  filter(length(unique(roost_date)) > 3) %>% # only include roosts used by someone on at least 3 nights
-  ggplot(aes(x = roost_date, y = indsHereTonight, col = roostID))+
-  geom_line()+
-  theme_minimal()+
-  theme(legend.position = "none") +
-  facet_wrap(~season, nrow = 3, scales = "free_x"))
-ggsave(indsHereTonight, file = "fig/indsHereTonight.png")
+    group_by(roostID) %>%
+    filter(length(unique(roost_date)) > 3) %>% # only include roosts used by someone on at least 3 nights
+    ggplot(aes(x = roost_date, y = indsHereTonight, col = roostID))+
+    geom_line()+
+    theme_minimal()+
+    theme(legend.position = "none") +
+    facet_wrap(~season, nrow = 3, scales = "free_x"))
+ggsave(indsHereTonight, file = "fig/indsHereTonight.png", width = 9, height = 5, dpi = 800)
 
 # Okay, we see that some roosts are consistently more populated than others. There are changes over time in which roosts are being used. But I don't immediately discern a pattern where different roosts keep turning into temporary hotspots, and then declining, etc. I think that's the kind of pattern that would be necessary in order for preferences to be driven according to roost desirability...?
 
@@ -224,20 +229,29 @@ durableDyads <- all %>%
 ddData <- all %>%
   filter(dyad %in% durableDyads)
 
-ddData %>%
-  mutate(season = factor(season, levels = c("2022_b", "2022_nb", "2023_b"))) %>%
-  group_by(dyad) %>%
-  arrange(season, .by_group = T) %>%
-  mutate(net = case_when(diffMnActual[3] > diffMnActual[1] ~ "inc",
-                         diffMnActual[3] < diffMnActual[1] ~ "dec")) %>%
-  ungroup() %>%
-  ggplot(aes(x = season, y = diffMnActual, group = dyad, col = net))+
-  geom_line(alpha = 0.2)+
-  theme_classic()+
-  facet_wrap(~net)
-  
-  
-  
-  
+(dyadIncDec <- ddData %>%
+    mutate(season = factor(season, levels = c("2022_b", "2022_nb", "2023_b"))) %>%
+    group_by(dyad) %>%
+    arrange(season, .by_group = T) %>%
+    mutate("Net change" = case_when(diffMnActual[3] > diffMnActual[1] ~ "+",
+                                    diffMnActual[3] < diffMnActual[1] ~ "-", 
+                                    TRUE ~ NA)) %>%
+    filter(!is.na(`Net change`)) %>%
+    ungroup() %>%
+    ggplot()+
+    geom_line(aes(x = season, y = diffMnActual, group = dyad, col = `Net change`),
+              linewidth = 0.1, alpha = 0.3)+
+    theme_classic()+
+    theme(legend.position = "none")+
+    scale_color_manual(values = c("firebrick", "skyblue3"))+
+    geom_hline(yintercept = 0, lty = 2)+
+    theme(text = element_text(size = 18))+
+    xlab("Season")+
+    ylab("Proportion differential (actual vs. mean random)")+
+    facet_wrap(~`Net change`))
+ggsave(dyadIncDec, file = "fig/dyadIncDec.png", width = 9, height = 7, dpi = 800)
+
+# 4. What differences are there between seasons, regardless of year? (Should )
+
 
 
